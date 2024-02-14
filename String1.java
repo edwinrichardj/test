cd47466312888ddd1942d1ff694ddb7ec27070f9
Edwin Richard J <106514394+edwinrichardj@users.noreply.github.com>
2024-02-15 04:35:26 +0530

    String1.java

--- String1.java
+++ String1.java
@@ -3,17 +3,19 @@ Output: abbccc
 Caution: Don't use inbuilt functions */
 import java.util.*;
 public class String1{
-  public static void main(System.in){
+  public static void main(String args[]){
     Scanner sc=new.Scanner(System.in);
     String str=sc.next();
     for(int i=0;i<str.length();i++){
-      if(str.charAt(i)>='0' && str.charAt(i)<=9){
+      if(str.charAt(i)>='0' && str.charAt(i)<='9'){
         int num=str.charAt(i)-'0';
         for(int j=0;j<num;j++){
           System.out.print(str.charAt(i-1));
         }
       }
+      else{
       System.out.print(str.charAt(i));
+      }
     }
   }
 }
