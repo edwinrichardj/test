9c7930a0df4b9326419bca0c9f6cf79011131833
Edwin Richard J <106514394+edwinrichardj@users.noreply.github.com>
2024-02-15 04:24:54 +0530

    Create String1.java

---
+++ String1.java
@@ -0,0 +1,19 @@
+/*Input: a1b2c3
+Output: abbccc
+Caution: Don't use inbuilt functions */
+import java.util.*;
+public class String1{
+  public static void main(System.in){
+    Scanner sc=new.Scanner(System.in);
+    String str=sc.next();
+    for(int i=0;i<str.length();i++){
+      if(str.charAt(i)>='0' && str.charAt(i)<=9){
+        int num=str.charAt(i)-'0';
+        for(int j=0;j<num;j++){
+          System.out.print(str.charAt(i-1));
+        }
+      }
+      System.out.print(str.charAt(i));
+    }
+  }
+}
